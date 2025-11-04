85f8443cd9f6b8ea032906cdd6687721eee95e7b
NormalV <moritz@fackler.cloud>
2025-11-04 09:43:52 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module net.normalv.golpeserver {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens net.normalv.golpeserver to javafx.fxml;
+    exports net.normalv.golpeserver;
+}
