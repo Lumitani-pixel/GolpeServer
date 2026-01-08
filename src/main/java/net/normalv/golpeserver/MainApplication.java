41e8ac352a06bf26a0ba49b2beb09fa95e91983d
NormalV <moritz@fackler.cloud>
2026-01-08 14:56:10 +0100

    Addition: ServerMenu.fxml and deckConfig.json all cards

--- src/main/java/net/normalv/golpeserver/MainApplication.java
+++ src/main/java/net/normalv/golpeserver/MainApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("ServerMenu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Golpe Server");
         stage.setScene(scene);
         stage.show();
     }
