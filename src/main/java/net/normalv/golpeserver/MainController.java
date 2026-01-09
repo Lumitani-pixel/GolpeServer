88dad7b100e646f3998452afc0ebfcb98a836640
NormalV <moritz@fackler.cloud>
2026-01-09 10:37:29 +0100

    addition: simple visual logging

--- src/main/java/net/normalv/golpeserver/MainController.java
+++ src/main/java/net/normalv/golpeserver/MainController.java
@@ -3,10 +3,20 @@ package net.normalv.golpeserver;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class MainController {
+    private static MainController instance;
+
     @FXML
     private Button startButton;
+
+    @FXML
+    private TextArea logs;
+
+    public MainController() {
+        instance = this;
+    }
 
     @FXML
     protected void onStartButtonClick() throws InterruptedException {
@@ -18,5 +28,13 @@ public class MainController {
             MainApplication.setServer(null);
             startButton.setText("Start");
         }
+    }
+
+    public void addTextToLog(String text) {
+        logs.appendText(text+"\n");
+    }
+
+    public static MainController getInstance() {
+        return instance;
     }
 }
--- src/main/java/net/normalv/golpeserver/websocket/Server.java
+++ src/main/java/net/normalv/golpeserver/websocket/Server.java
@@ -1,11 +1,13 @@
 package net.normalv.golpeserver.websocket;
 
+import net.normalv.golpeserver.MainController;
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 
 import java.net.InetSocketAddress;
 
+//TODO implement better logging
 public class Server extends WebSocketServer {
     public Server(InetSocketAddress socketAddress) {
         super(socketAddress);
@@ -14,25 +16,30 @@ public class Server extends WebSocketServer {
     @Override
     public void onOpen(WebSocket webSocket, ClientHandshake clientHandshake) {
         System.out.println("New connection: "+webSocket.getRemoteSocketAddress());
+        MainController.getInstance().addTextToLog("New connection: "+webSocket.getRemoteSocketAddress());
     }
 
     @Override
     public void onClose(WebSocket webSocket, int code, String reason, boolean remote) {
         System.out.println(webSocket.getRemoteSocketAddress()+" disconnected because of "+reason+". Exit code: "+code);
+        MainController.getInstance().addTextToLog(webSocket.getRemoteSocketAddress()+" disconnected because of "+reason+". Exit code: "+code);
     }
 
     @Override
     public void onMessage(WebSocket webSocket, String message) {
         System.out.println("Got message from: "+webSocket.getRemoteSocketAddress()+". Content: "+message);
+        MainController.getInstance().addTextToLog("Got message from: "+webSocket.getRemoteSocketAddress()+". Content: "+message);
     }
 
     @Override
     public void onError(WebSocket webSocket, Exception e) {
         System.out.println(e.getMessage()+" on "+webSocket.getRemoteSocketAddress());
+        MainController.getInstance().addTextToLog(e.getMessage()+" on "+webSocket.getRemoteSocketAddress());
     }
 
     @Override
     public void onStart() {
         System.out.println("Started Golpe Server instance");
+        MainController.getInstance().addTextToLog("Started Golpe Server instance");
     }
 }
