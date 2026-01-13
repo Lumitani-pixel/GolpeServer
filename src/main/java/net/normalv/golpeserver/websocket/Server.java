18392955874755fe79725b1fe2cdae1ebb26fd7e
NormalV <moritz@fackler.cloud>
2026-01-13 13:03:19 +0100

    cleanup: logging is simplified in Server

--- src/main/java/net/normalv/golpeserver/websocket/Server.java
+++ src/main/java/net/normalv/golpeserver/websocket/Server.java
@@ -15,31 +15,31 @@ public class Server extends WebSocketServer {
 
     @Override
     public void onOpen(WebSocket webSocket, ClientHandshake clientHandshake) {
-        System.out.println("New connection: "+webSocket.getRemoteSocketAddress());
-        MainController.getInstance().addTextToLog("New connection: "+webSocket.getRemoteSocketAddress());
+        log("New connection: "+webSocket.getRemoteSocketAddress());
     }
 
     @Override
     public void onClose(WebSocket webSocket, int code, String reason, boolean remote) {
-        System.out.println(webSocket.getRemoteSocketAddress()+" disconnected because of "+reason+". Exit code: "+code);
-        MainController.getInstance().addTextToLog(webSocket.getRemoteSocketAddress()+" disconnected because of "+reason+". Exit code: "+code);
+        log(webSocket.getRemoteSocketAddress()+" disconnected because of "+reason+". Exit code: "+code);
     }
 
     @Override
     public void onMessage(WebSocket webSocket, String message) {
-        System.out.println("Got message from: "+webSocket.getRemoteSocketAddress()+". Content: "+message);
-        MainController.getInstance().addTextToLog("Got message from: "+webSocket.getRemoteSocketAddress()+". Content: "+message);
+        log("Got message from: "+webSocket.getRemoteSocketAddress()+". Content: "+message);
     }
 
     @Override
     public void onError(WebSocket webSocket, Exception e) {
-        System.out.println(e.getMessage()+" on "+webSocket.getRemoteSocketAddress());
-        MainController.getInstance().addTextToLog(e.getMessage()+" on "+webSocket.getRemoteSocketAddress());
+        log(e.getMessage()+" on "+webSocket.getRemoteSocketAddress());
     }
 
     @Override
     public void onStart() {
-        System.out.println("Started Golpe Server instance");
-        MainController.getInstance().addTextToLog("Started Golpe Server instance");
+        log("Started Golpe Server");
+    }
+
+    public static void log(String msg) {
+        System.out.println(msg);
+        MainController.getInstance().addTextToLog(msg);
     }
 }
