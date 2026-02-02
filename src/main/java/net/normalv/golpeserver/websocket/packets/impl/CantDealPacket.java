5199cea5cb2011c5db05e5809bd412dc7d3e5f46
NormalV <moritz@fackler.cloud>
2026-02-02 18:12:37 +0100

    improv: name change for multi use

--- src/main/java/net/normalv/golpeserver/websocket/packets/impl/CantDealPacket.java
+++ src/main/java/net/normalv/golpeserver/websocket/packets/impl/CantDealPacket.java
@@ -2,8 +2,8 @@ package net.normalv.golpeserver.websocket.packets.impl;
 
 import net.normalv.golpeserver.websocket.packets.Packet;
 
-public class RequestCardPacket extends Packet {
-    public RequestCardPacket() {
-        super("request_card");
+public class CantDealPacket extends Packet {
+    public CantDealPacket() {
+        super("cant_deal");
     }
 }
